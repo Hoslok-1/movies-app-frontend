5c52f0de1ea88a8034e5b2dc3764b7a192dfaa33
Anuj <anujhoslokdev@gmail.com>
2023-11-24 20:01:52 +0530

    major restructuring of architecture within the app, this was done to accomadate ReactRouterDOM'

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { Route, RouterProvider, createBrowserRouter,createRoutesFromElements } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path=''></Route>
+    </Route>
+  )
+)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 
