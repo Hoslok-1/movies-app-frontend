18ee93d1e464a51d1f5a2ea4cd5f839d1005e301
Anuj <anujhoslokdev@gmail.com>
2023-12-10 18:06:46 +0530

    fixed bug with the carousel which made the movies go from top to bottom instead of left to right

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,26 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Register.css'
 const Register = () => {
+
+  const [formData,setFormData] = useState({
+    email:"",
+    password:""
+  });
+
+  console.log(formData);
+  const handleChange = (e:any) => {
+    const {name,value} = e.target;
+    setFormData({
+      ...formData,
+      [name]:value
+    });
+  };
+
+  const handleSubmit = (e:any) => {
+    e.preventDefault();
+    console.log("working");
+  }
+
+
   return (
     <div className="registration-container">
       <form className="registration-form">
         <h2>Create an Account to Book Your Favorite Movies</h2>
-        <div className="form-group">
-          <label htmlFor="username">Username:</label>
-          <input type="text" id="username" name="username" required />
-        </div>
 
         <div className="form-group">
           <label htmlFor="email">Email:</label>
-          <input type="email" id="email" name="email" required />
+          <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} required />
         </div>
 
         <div className="form-group">
           <label htmlFor="password">Password:</label>
-          <input type="password" id="password" name="password" required />
+          <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} required />
         </div>
 
-        <button type="submit">Register</button>
+        <button type="submit" onClick={handleSubmit}>Register</button>
       </form>
     </div>
   )
